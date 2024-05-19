0321427787d25c9db28e410a4e20eedfd74e0a31
TClaypool00 <claypooltyler00@gmail.com>
2024-05-19 17:46:07 -0500

    Added Login View models

---
+++ ShoelessJoe/ShoelessJoe.App/Models/LoginViewModel.cs
@@ -0,0 +1,17 @@
+﻿using ShoelessJoe.App.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoelessJoe.App.Models
+{
+    public class LoginViewModel
+    {
+        [ShoelessJoeRequired]
+        [ShoelessJoeMaxLength]
+        public string Email { get; set; }
+
+        [ShoelessJoeRequired]
+        [ShoelessJoeMaxLength]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
---
+++ ShoelessJoe/ShoelessJoe.App/Models/UserViewModel.cs
@@ -0,0 +1,27 @@
+﻿using ShoelessJoe.App.Attributes;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoelessJoe.App.Models
+{
+    public class UserViewModel : LoginViewModel
+    {
+        public int UserId { get; set; }
+
+        [Display(Name = "First Name")]
+        [ShoelessJoeRequired]
+        [ShoelessJoeMaxLength]
+        public string FirstName { get; set; }
+
+        [Display(Name = "Last Name")]
+        [ShoelessJoeRequired]
+        [ShoelessJoeMaxLength]
+        public string LastName { get; set; }
+
+        [Display(Name = "Phone Number")]
+        [ShoelessJoeRequired]
+        [ShoelessJoeMaxLength(10)]
+        [DataType(DataType.PhoneNumber, ErrorMessage = "Must be a valid phone number")]
+        public string PhoneNumb { get; set; }
+        public bool IsAdmin { get; set; }
+    }
+}
