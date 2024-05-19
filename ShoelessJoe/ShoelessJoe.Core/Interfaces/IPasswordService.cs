7b37ebc1dc2322928b2129bf15adff2db46a88b5
TClaypool00 <claypooltyler00@gmail.com>
2024-05-19 17:43:24 -0500

    Added Bcrypt and password service

---
+++ ShoelessJoe/ShoelessJoe.Core/Interfaces/IPasswordService.cs
@@ -0,0 +1,12 @@
+﻿namespace ShoelessJoe.Core.Interfaces
+{
+    public interface IPasswordService
+    {
+        public string InvalidPasswordMessage { get; }
+        public string PasswordDoesNotMeetRequirementsMessage { get; }
+
+        public string HashPassword(string password);
+        public bool VerifyPassword(string text, string password);
+        public bool PasswordMeetsRequirements(string password);
+    }
+}
---
+++ ShoelessJoe/ShoelessJoe.DataAccess/Services/PasswordService.cs
@@ -0,0 +1,28 @@
+﻿using ShoelessJoe.Core.Interfaces;
+using System.Text.RegularExpressions;
+
+namespace ShoelessJoe.DataAccess.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        //TODO: Put strings in it's own AppSettings
+        public string InvalidPasswordMessage => "Invalid Password";
+
+        public string PasswordDoesNotMeetRequirementsMessage => "Password does not meet our requirements";
+
+        public string HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public bool PasswordMeetsRequirements(string password)
+        {
+            return Regex.IsMatch(password, @"^(.{8,20}|[^0-9]*|[^A-Z])$");
+        }
+
+        public bool VerifyPassword(string text, string password)
+        {
+            return BCrypt.Net.BCrypt.Verify(text, password);
+        }
+    }
+}
