fe0e3de9270317eb6d5d854a7295983fa62579d0
TClaypool00 <claypooltyler00@gmail.com>
2024-05-19 17:40:09 -0500

    Added attributes to reduce code redundency

---
+++ ShoelessJoe/ShoelessJoe.App/Attributes/ShoelessJoeMaxLength.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoelessJoe.App.Attributes
+{
+    public class ShoelessJoeMaxLength : MaxLengthAttribute
+    {
+        public ShoelessJoeMaxLength() : base(255)
+        {
+
+        }
+
+        public ShoelessJoeMaxLength(int length) : base(length)
+        {
+
+        }
+
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            ErrorMessage = $"{validationContext.DisplayName} has a max length of {Length} characters";
+
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
---
+++ ShoelessJoe/ShoelessJoe.App/Attributes/ShoelessJoeRequired.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ShoelessJoe.App.Attributes
+{
+    public class ShoelessJoeRequired : RequiredAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            ErrorMessage = $"{validationContext.DisplayName} is a required field";
+
+            return base.IsValid(value, validationContext);
+        }
+    }
+}
