4596801fce19398f2b744184a978cf1324077323
TClaypool00 <claypooltyler00@gmail.com>
2024-05-19 17:44:19 -0500

    Added Helper for Controllers

---
+++ ShoelessJoe/ShoelessJoe.App/Controllers/ControllerHelper.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ShoelessJoe.App.Controllers
+{
+    public abstract class ControllerHelper : Controller
+    {
+        protected void SetSuccessMessage(string message)
+        {
+            ViewBag.Success = message;
+        }
+
+        protected void SetErrorMessage(string message)
+        {
+            ViewBag.Error = message;
+        }
+
+        protected void SetTempSuccessMessage(string message)
+        {
+            TempData["Success"] = message;
+        }
+
+        protected void GetTempSuccessMessage()
+        {
+            if (TempData["Success"] is not null)
+            {
+                ViewBag.Success = TempData["Success"];
+            }
+        }
+    }
+}
