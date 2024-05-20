9e04f4064def41ff175180b7649d60b85fed9bf8
TClaypool00 <claypooltyler00@gmail.com>
2024-05-19 21:03:56 -0500

    Added functionality to add shoe(s) no post yet

---
+++ ShoelessJoe/ShoelessJoe.App/Controllers/ShoesController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoelessJoe.App.Models.PostModels;
+using ShoelessJoe.Core.Interfaces;
+
+namespace ShoelessJoe.App.Controllers
+{
+    [Authorize]
+    public class ShoesController : ControllerHelper
+    {
+        private readonly IManufacterService _manufacterService;
+        private readonly IModelService _modelService;
+
+        public ShoesController(IManufacterService manufacterService, IModelService modelService)
+        {
+            _manufacterService = manufacterService;
+            _modelService = modelService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Add()
+        {
+            var model = new PostShoeViewModel
+            {
+                ModelDropDown = await _modelService.GetModelDropDown(UserId),
+                ManufacterDropDown = await _manufacterService.GetCoreManufacterDropDown(UserId)
+            };
+
+            return View(model);
+        }
+    }
+}
---
+++ ShoelessJoe/ShoelessJoe.App/Models/PostModels/PostShoeViewModel.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using ShoelessJoe.App.Attributes.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoelessJoe.App.Models.PostModels
+{
+    public class PostShoeViewModel
+    {
+        [Display(Name = "Right Shoe Size")]
+        public double? RightSize { get; set; }
+        [Display(Name = "Left Shoe Size")]
+        public double? LeftSize { get; set; }
+
+        [Display(Name = "Model selection")]
+        [ShoelessJoeRequired]
+        public int? ModelId { get; set; }
+        public List<SelectListItem> ModelDropDown { get; set; }
+
+        [Display(Name = "Manufacter selection")]
+        [ShoelessJoeRequired]
+        public int? ManufacterId { get; set; }
+        public List<SelectListItem> ManufacterDropDown { get; set; }
+
+        [Display(Name = "Left Shoe 1")]
+        public IFormFile LeftShoeFile1 { get; set; }
+        [Display(Name = "Left Shoe 2")]
+        public IFormFile LeftShoeFile2 { get; set; }
+
+        [Display(Name = "Right Shoe 1")]
+        public IFormFile RightShoeFile1 { get; set; }
+
+        [Display(Name = "Right Shoe 2")]
+        public IFormFile RightShoeFile2 { get; set; }
+    }
+}
