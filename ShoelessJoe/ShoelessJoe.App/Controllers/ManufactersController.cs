db2e8e31bdeae80e393bfc15e190c9777eabe71f
TClaypool00 <claypooltyler00@gmail.com>
2024-06-03 17:12:00 -0500

    Created Model and Controller to add a manufacter

---
+++ ShoelessJoe/ShoelessJoe.App/Controllers/ManufactersController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using ShoelessJoe.App.Models;
+using ShoelessJoe.Core.Interfaces;
+
+namespace ShoelessJoe.App.Controllers
+{
+    [Authorize]
+    public class ManufactersController : ControllerHelper
+    {
+        private readonly IManufacterService _manufacterService;
+
+        public ManufactersController(IManufacterService manufacterService)
+        {
+            _manufacterService = manufacterService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Add([FromBody] ManufacterViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return DisplayErrors();
+            }
+
+            if (await _manufacterService.ManufacterExistByName(model.ManufacturerName, UserId))
+            {
+                return BadRequest(_manufacterService.ManufacterNameExistsMessage);
+            }
+
+            var coreManufacter = Mapper.MapManufacter(model, UserId);
+
+            coreManufacter = await _manufacterService.AddManufacterAsync(coreManufacter);
+
+            model.ManufacturerId = coreManufacter.ManufacterId;
+
+            return Ok(model);
+        }
+    }
+}
---
+++ ShoelessJoe/ShoelessJoe.App/Models/ManufacterViewModel.cs
@@ -0,0 +1,16 @@
+﻿using ShoelessJoe.App.Attributes.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoelessJoe.App.Models
+{
+    public class ManufacterViewModel
+    {
+        [Display(Name = "Manufacturer Id")]
+        public int ManufacturerId { get; set; }
+
+        [Display(Name = "Manufacturer Name")]
+        [ShoelessJoeRequired]
+        [ShoelessJoeMaxLength]
+        public string ManufacturerName { get; set; }
+    }
+}
