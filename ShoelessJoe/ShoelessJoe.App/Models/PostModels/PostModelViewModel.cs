b97e765c30e504db7678993c32b88d7a9e472d16
TClaypool00 <claypooltyler00@gmail.com>
2024-06-03 17:06:49 -0500

    Puts files into a list
    Logc to create file name, convert it into a byte array, and an IFormFile

---
+++ ShoelessJoe/ShoelessJoe.App/Models/PostModels/PostModelViewModel.cs
@@ -0,0 +1,19 @@
+﻿using ShoelessJoe.App.Attributes.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace ShoelessJoe.App.Models.PostModels
+{
+    public class PostModelViewModel
+    {
+        public int ModelId { get; set; }
+
+        [Display(Name = "Model Name")]
+        [ShoelessJoeRequired]
+        [ShoelessJoeMaxLength]
+        public string ModelName { get; set; }
+
+        [Display(Name = "Manufacter Id")]
+        [ShoelessJoeRequired]
+        public int ManufacterId { get; set; }
+    }
+}
--- ShoelessJoe/ShoelessJoe.Core/CoreModels/CoreShoeImage.cs
+++ ShoelessJoe/ShoelessJoe.Core/CoreModels/CoreShoeImage.cs
@@ -4,27 +4,84 @@ namespace ShoelessJoe.Core.CoreModels
 {
     public class CoreShoeImage
     {
+        private readonly MemoryStream _memoryStream;
+        private readonly byte[] _shoeArray;
+
+        private string _fileName;
+        private IFormFile _formFile;
+
         public int ShoeImageId { get; set; }
 
-        public IFormFile RightShoeImage1 { get; set; }
+        public string FileName
+        {
+            get
+            {
+                return _fileName;
+            }
+            set
+            {
+                _fileName = value;
+            }
+        }
+        public IFormFile File
+        {
+            get
+            {
+                return _formFile;
+            }
+            set
+            {
+                _formFile = value;
+            }
+        }
 
-        public IFormFile RightShoeImage2 { get; set; }
-
-        public IFormFile LeftShoeImage1 { get; set; }
-
-        public IFormFile LeftShoeImage2 { get; set; }
-
-        public string RightShoeImage1Path { get; set; }
-
-        public string RightShoeImage2Path { get; set; }
-
-        public string LeftShoeImage1Path { get; set; }
-
-        public string LeftShoeImage2Path { get; set; }
-
-        public string PicturePath { get; set; }
+        public byte[] ShoeArray
+        {
+            get
+            {
+                return _shoeArray;
+            }
+        }
 
         public int ShoeId { get; set; }
         public CoreShoe Shoe { get; set; }
+
+        public CoreShoeImage()
+        {
+
+        }
+
+        public CoreShoeImage(byte[] bytes, string fileName)
+        {
+            _shoeArray = bytes;
+            _fileName = fileName;
+
+            _memoryStream = new MemoryStream(bytes);
+
+            File = new FormFile(_memoryStream, 0, _shoeArray.Length, "Name", _fileName);
+
+            _memoryStream.Dispose();
+        }
+
+        public CoreShoeImage(IFormFile formFile)
+        {
+            _formFile = formFile;
+
+            using (_memoryStream = new MemoryStream())
+            {
+                _formFile.CopyTo(_memoryStream);
+                _shoeArray = _memoryStream.ToArray();
+            }
+
+            GenerateNewFile();
+        }
+
+        private void GenerateNewFile()
+        {
+            string justFileName = Path.GetFileNameWithoutExtension(_formFile.FileName);
+            string extension = Path.GetExtension(_formFile.FileName);
+
+            _fileName = $"{justFileName}-{Guid.NewGuid()}{extension}";
+        }
     }
 }
