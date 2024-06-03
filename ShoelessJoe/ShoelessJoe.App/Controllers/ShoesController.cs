1bd97c9ec8d141e5b1d7e3fa1aabf4d68ab37437
TClaypool00 <claypooltyler00@gmail.com>
2024-06-03 17:11:30 -0500

    Can now add Shoe to  database

--- ShoelessJoe/ShoelessJoe.App/Controllers/ShoesController.cs
+++ ShoelessJoe/ShoelessJoe.App/Controllers/ShoesController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using ShoelessJoe.App.Models.PostModels;
 using ShoelessJoe.Core.Interfaces;
 
@@ -10,11 +11,16 @@ namespace ShoelessJoe.App.Controllers
     {
         private readonly IManufacterService _manufacterService;
         private readonly IModelService _modelService;
+        private readonly IShoeService _shoeService;
+        private static List<SelectListItem> _manufacterDropDown;
+        private static List<SelectListItem> _modelDropDown;
 
-        public ShoesController(IManufacterService manufacterService, IModelService modelService)
+
+        public ShoesController(IManufacterService manufacterService, IModelService modelService, IShoeService shoeService)
         {
             _manufacterService = manufacterService;
             _modelService = modelService;
+            _shoeService = shoeService;
         }
 
         [HttpGet]
@@ -22,11 +28,41 @@ namespace ShoelessJoe.App.Controllers
         {
             var model = new PostShoeViewModel
             {
-                ModelDropDown = await _modelService.GetModelDropDown(UserId),
+                ModelDropDown = await _modelService.GetModelDropDown(),
                 ManufacterDropDown = await _manufacterService.GetCoreManufacterDropDown(UserId)
             };
 
+            _manufacterDropDown = model.ManufacterDropDown;
+            _modelDropDown = model.ModelDropDown;
+
             return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Add([FromForm] PostShoeViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.ManufacterDropDown = _manufacterDropDown;
+                model.ModelDropDown = _modelDropDown;
+
+                return View(model);
+            }
+
+            model.ToList();
+
+            var coreShoe = Mapper.MapShoe(model);
+
+            coreShoe =  await _shoeService.AddShoeAsync(coreShoe);
+
+            SetTempSuccessMessage(_shoeService.ShoeAddedMessage);
+
+            return RedirectToAction("View", new
+            {
+                Id = coreShoe.ShoeId
+            });
+
         }
     }
 }
