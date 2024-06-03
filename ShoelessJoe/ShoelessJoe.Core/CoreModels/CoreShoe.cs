20ac43a093f796caaf4f01cadd07e9f05de0b68a
TClaypool00 <claypooltyler00@gmail.com>
2024-06-03 17:06:11 -0500

    Puting IFormFile items in a list

--- ShoelessJoe/ShoelessJoe.Core/CoreModels/CoreShoe.cs
+++ ShoelessJoe/ShoelessJoe.Core/CoreModels/CoreShoe.cs
@@ -1,7 +1,11 @@
-﻿namespace ShoelessJoe.Core.CoreModels
+﻿using Microsoft.AspNetCore.Http;
+
+namespace ShoelessJoe.Core.CoreModels
 {
     public class CoreShoe
     {
+        private List<IFormFile> _files;
+
         public int ShoeId { get; set; }
 
         public double? RightSize { get; set; }
@@ -13,8 +17,28 @@
         public int ModelId { get; set; }
         public CoreModel Model { get; set; }
 
-        public CoreShoeImage ShoeImage { get; set; }
+        public List<CoreShoeImage> ShoeImages { get; set; }
 
         public List<CorePotentialBuy> PotentialBuys { get; set; }
+
+        public CoreShoe()
+        {
+
+        }
+
+        public CoreShoe(List<IFormFile> files)
+        {
+            _files = files;
+
+            if (_files is not null && _files.Count > 0)
+            {
+                ShoeImages = new List<CoreShoeImage>();
+
+                for (int i = 0; i < _files.Count; i++)
+                {
+                    ShoeImages.Add(new CoreShoeImage(_files[i]));
+                }
+            }
+        }
     }
 }
