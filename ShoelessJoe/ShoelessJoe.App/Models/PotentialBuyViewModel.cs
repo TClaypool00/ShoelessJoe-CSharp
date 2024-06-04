13393168cbaa9444c2990602b4ef73207eaea59b
TClaypool00 <claypooltyler00@gmail.com>
2024-06-04 13:32:08 -0500

    Added View Model for PotentialBuy object

---
+++ ShoelessJoe/ShoelessJoe.App/Models/PotentialBuyViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace ShoelessJoe.App.Models
+{
+    public class PotentialBuyViewModel
+    {
+        public int PotentialBuyId { get; set; }
+        public string UserDisplayName { get; set; }
+        public int UserId { get; set; }
+    }
+}
